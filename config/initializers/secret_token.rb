60ebbc63afb9c941fd455e6c1b69561ae66eedb4
Philipp Grundmann <philippgrundmann@gmx.net>
2012-07-18 21:07:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssTrends::Application.config.secret_token = 'a93a4e90ddd5ff20d089b3e4190821d35af59d2e1ad5a868cba372b881b723416ea9c25fa4fb64d0727f1a26ba1a0f927219ed5e2702438cc88fdbb870603371'
