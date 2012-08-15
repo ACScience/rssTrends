379973cd503035cf8e11478d3d73e916ba8588e9
stefan <l00ck.1337@gmail.com>
2012-08-15 22:31:36 +0200

    Alles umbennen... nameconventions :P

---
+++ app/controllers/feed_entries_controller.rb
@@ -0,0 +1,21 @@
+class FeedEntriesController < ApplicationController
+	def index
+	  @feed_entries = FeedEntry.all 
+	end
+	
+	def show
+      @feed_entry = FeedEntry.find(params[:id])
+      
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @feed }
+    end
+  end
+
+	def new
+			@feed_entry = FeedEntry.update_from_feed("http://www.spiegel.de/politik/index.rss")				
+			
+			redirect_to '/feeds.html'
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RssTrends::Application.routes.draw do
 
-	resources :feeds
+	resources :feed_entries
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
