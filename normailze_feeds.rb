7281be5af4f97b5716c4a9cdb29bdf93180cb510
l00ck <l00ck.1337@gmail.com>
2012-08-15 10:39:39 +0200

    leerzeichen zuviel :D

--- normailze_feeds.rb
+++ normailze_feeds.rb
@@ -1,3 +1,3 @@
 def normalize(s)
-    s.downcase.gsub(/[ä]/, "ae").gsub(/[ö]/, "oe").gsub(/[ü]/,   "ue").gsub(/[ß]/, "ss").gsub(/[^a-zA-Z\ ]/,"").split
+    s.downcase.gsub(/[ä]/, "ae").gsub(/[ö]/, "oe").gsub(/[ü]/, "ue").gsub(/[ß]/, "ss").gsub(/[^a-zA-Z\ ]/,"").split
 end
