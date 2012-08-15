b5b0270bd7899310ca84a2c6c614e5a64d552d62
Philipp Grundmann <philippgrundmann@gmx.net>
2012-08-15 17:39:24 +0200

    added german stopword list

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,4 +6,8 @@ class FeedsController < ApplicationController
 	def show
         @feed = FeedEntry.find(params[:id])
     end
+
+	def new
+			FeedEntry.update_from_feed("http://www.spiegel.de/politik/index.rss")
+	end
 end
