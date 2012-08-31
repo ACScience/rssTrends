276dca23d1d43775d878c895cb0e13ad5932a5f2
stefan <l00ck.1337@gmail.com>
2012-08-31 09:14:19 +0200

    Category Spalte in FeedEntry hinzugefügt

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120830143850) do
+ActiveRecord::Schema.define(:version => 20120831071027) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -53,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20120830143850) do
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
     t.boolean  "processed",    :default => false
+    t.string   "category"
   end
 
   create_table "feed_origins", :force => true do |t|
