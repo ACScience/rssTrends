1f337357e07acec538d8977df943c1df48e40bb7
stefan <l00ck.1337@gmail.com>
2012-09-07 17:14:49 +0200

    json und XML hinzugefügt und IDs aus den views gelöscht

--- app/controllers/feed_entries_controller.rb
+++ app/controllers/feed_entries_controller.rb
@@ -1,19 +1,25 @@
 class FeedEntriesController < ApplicationController
-	
+
 	def index
-  if(params.has_key?(:category))
-	  @feed_entries = FeedEntry.where("category = ?", params[:category]).paginate(:page => params[:page], :per_page => 15).order("published_at DESC")
-  else
-  @feed_entries = FeedEntry.paginate(:page => params[:page], :per_page => 15).order("published_at DESC")
-  end
-end
+		if(params.has_key?(:category))
+			@feed_entries = FeedEntry.where("category = ?", params[:category]).paginate(:page => params[:page], :per_page => 15).order("published_at DESC")
+		else
+			@feed_entries = FeedEntry.paginate(:page => params[:page], :per_page => 15).order("published_at DESC")
+		end
+		respond_to do |format|
+		format.html # show.html.erb
+		format.json { render :json => @feed_entries }
+		format.xml { render :xml => @feed_entries }
+    end
+	end
 	
 	def show
       @feed_entry = FeedEntry.find(params[:id])
       
       respond_to do |format|
       format.html # show.html.erb
-      format.json { render :json => @feed }
+      format.json { render :json => @feed_entry }
+      format.xml { render :xml => @feed_entry }
     end
   end
 
--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,17 +1,28 @@
 class TrendsController < ApplicationController
   def index
-	if(params.has_key?(:category))
-	  @trends = Trend.where("category = ?", params[:category]).paginate(:page => params[:page], :per_page => 15).order("counter DESC")
-	else
-	@trends = Trend.where(:category => "Allgemein").paginate(:page => params[:page], :per_page => 15).order("counter DESC")
-	end
+    if(params.has_key?(:category))
+      @trends = Trend.where("category = ?", params[:category]).paginate(:page => params[:page], :per_page => 15).order("counter DESC")
+    else
+      @trends = Trend.where(:category => "Allgemein").paginate(:page => params[:page], :per_page => 15).order("counter DESC")
+    end
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @trends }
+      format.xml { render :xml => @trends }
+    end
   end
 
 
   def show
-  @trend = Trend.find(params[:id])
-  @relation = Relation.find(:all,:conditions => {:trend_id => @trend.id})
-  @feed_entries = FeedEntry.all
+    @trend = Trend.find(params[:id])
+    @relation = Relation.find(:all,:conditions => {:trend_id => @trend.id})
+    @feed_entries = FeedEntry.all
+
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @trend }
+      format.xml { render :xml => @trend}
+    end
   end
 
   def destroy
