6ad2f8e9295bc07717adef5b3432f35f16fade96
Philipp Grundmann <philippgrundmann@gmx.net>
2012-09-07 19:51:32 +0200

    added check to make sure that single characters will not be saved as potential trendwords

--- app/models/trend_gen.rb
+++ app/models/trend_gen.rb
@@ -23,63 +23,62 @@ class TrendGen < FeedPrep
 				# Abgleich potentieller Trendwörter mit der Datenbank
 				normalized_feed.each do |w|
 					w.to_s
-					double = Trend.all
-					double.each do |d|
-					currentTrendId = d.id
-						# Update eines bestehenden Trends (Trendwort existiert schon in der Datenbank)
-						if (d.trendy_word == w) == true && (d.category == currentFeedCat) == true
-							d.update_attributes(:counter => d.counter + 1)
-							relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
-							relation.save
-							puts "plus1"
+					# Trend wird nur erstellt wenn das potentielle Trendwort aus mehr als einem Buchstaben besteht
+					if w.length != 0
+						double = Trend.all
+						double.each do |d|
+						currentTrendId = d.id
+							# Update eines bestehenden Trends (Trendwort existiert schon in der Datenbank)
+							if (d.trendy_word == w) == true && (d.category == currentFeedCat) == true
+								d.update_attributes(:counter => d.counter + 1)
+								relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
+								relation.save
+								puts "plus1"
 
-								#generalTrend = Trend.all
-								#generalTrend.each do |gT|
 								gT = Trend.find_by_trendy_word_and_category(w, "Allgemein")
 								currentTrendId = gT.id								
 								# Kein zusätzlicher Check nötig, da es schon in einer anderen Kategorie existiert => und somit auch als allgemeiner Trend								
 								gT.update_attributes(:counter => gT.counter + 1)
 								relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
 								relation.save										
-								#end
+								
+								w = "nil"
 
-							w = "nil"
-
+							end
 						end
-					end
-					# Anlegen eines neuen Trends (Trendwort noch nicht in der Datenbank vorhanden)
-					unless w == "nil"
-						trend = Trend.new(:trendy_word => w, :category => currentFeedCat)
-						trend.save
-						currentTrend = Trend.find_by_trendy_word_and_category(w, currentFeedCat)
-						currentTrendId = currentTrend.id
-						relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
-							relation.save
-						puts "gespeichert"
+							# Anlegen eines neuen Trends (Trendwort noch nicht in der Datenbank vorhanden)
+							unless w == "nil"
+								trend = Trend.new(:trendy_word => w, :category => currentFeedCat)
+								trend.save
+								currentTrend = Trend.find_by_trendy_word_and_category(w, currentFeedCat)
+								currentTrendId = currentTrend.id
+								relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
+								relation.save
+								puts "gespeichert"
 		
-							gT = Trend.find_by_trendy_word_and_category(w, "Allgemein")
-								if gT != nil
-									currentTrendId = gT.id
-									gT.update_attributes(:counter => gT.counter + 1)
-									relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
-									relation.save
-								else
-									gT = Trend.new(:trendy_word => w, :category => "Allgemein")
-									gT.save
-									currentTrend = Trend.find_by_trendy_word_and_category(w, "Allgemein")
-									currentTrendId = currentTrend.id
-									relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
-									relation.save
+								gT = Trend.find_by_trendy_word_and_category(w, "Allgemein")
+									if gT != nil
+										currentTrendId = gT.id
+										gT.update_attributes(:counter => gT.counter + 1)
+										relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
+										relation.save
+									else
+										gT = Trend.new(:trendy_word => w, :category => "Allgemein")
+										gT.save
+										currentTrend = Trend.find_by_trendy_word_and_category(w, "Allgemein")
+										currentTrendId = currentTrend.id
+										relation = Relation.new(:feed_entry_id => currentFeedId, :trend_id => currentTrendId)
+										relation.save
+									end
 								end
-
-					end			
-				end
+							end			
+						end
 				
-				# Markieren der Feeds aus denen bereits Trends generiert, bzw. upgedated wurden (ermöglicht das Erkennen der neu hinzugekommenen Feeds nach einem Feed-Update)
-				feed_entry.update_attributes(:processed => true)
-				puts feed_entry.processed
-				puts feed_entry.id
-			end
+					# Markieren der Feeds aus denen bereits Trends generiert, bzw. upgedated wurden (ermöglicht das Erkennen der neu hinzugekommenen Feeds nach einem Feed-Update)
+					feed_entry.update_attributes(:processed => true)
+					puts feed_entry.processed
+					puts feed_entry.id
+				end
 		end
 	end
 	
