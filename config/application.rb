21e973f9e0febf70483e5dd494550455ab34d001
Philipp Grundmann <philippgrundmann@gmx.net>
2012-09-26 15:24:54 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module RssTrends
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-    Rails.env = ActiveSupport::StringInquirer.new('production')
+    #Rails.env = ActiveSupport::StringInquirer.new('production')
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
